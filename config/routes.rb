ec01003de3a70ab75ac96071334da7811dbeba1e
Ben S <45582717+nextbendev@users.noreply.github.com>
2020-07-01 18:27:08 -0400

    first com

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  resources :categories
+  resources :blogs
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
