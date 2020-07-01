5c85221ad266023cd2805c93f66a0528cf224411
Ben S <45582717+nextbendev@users.noreply.github.com>
2020-07-01 19:24:31 -0400

    fill in

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,16 +1,16 @@
-ActiveAdmin.register Blog do
+ActiveAdmin.register Category do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :title, :content, :category_id
+  permit_params :category
   #
   # or
   #
   # permit_params do
-  #   permitted = [:title, :content, :category_id]
+  #   permitted = [:category]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
